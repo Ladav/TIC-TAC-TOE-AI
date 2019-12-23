589e4531f37d8bc59ac753f9aca1694418b67bed
Davinder Singh <davindersaini003@gmail.com>
2019-12-23 23:43:58 +0530

    final

--- js/script.js
+++ js/script.js
@@ -205,6 +205,8 @@ const UIController = (() => {
 
             // highlight active player
             DOMInput[activePlayer].classList.add('active');
+            const otherPlayer = activePlayer === 'X' ? 'O' : 'X';
+            DOMInput[otherPlayer].classList.remove('active');
 
             // remove the congrats message from the previous gameplay
             DOMInput.window.style.display = 'none';
