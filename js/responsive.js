ca0b801652a3f52508a224d1acff7d23c1ec726b
Davinder Singh <davindersaini003@gmail.com>
2020-02-13 18:37:45 +0530

    final

--- js/responsive.js
+++ js/responsive.js
@@ -1,3 +1,7 @@
-const appHeight = () => document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`)
-window.addEventListener('resize', appHeight);
+const appHeight = () => document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`);
 appHeight();
+
+// We listen to the resize event
+window.addEventListener('resize', () => {
+    document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`);
+});
